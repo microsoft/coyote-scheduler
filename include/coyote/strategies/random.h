29176661e8435b137250fec930fc9e87ec4f3da1
Pantazis Deligiannis <pdeligia@microsoft.com>
2020-11-05 13:46:54 -0800

    edit

--- include/coyote/strategies/random.h
+++ include/coyote/strategies/random.h
@@ -12,11 +12,11 @@ namespace coyote
 	class Random
 	{
 	private:
-        static constexpr unsigned STATE_BITS = 8 * sizeof(uint64_t);
-        static constexpr unsigned RESULT_BITS = 8 * sizeof(uint64_t);
+		static constexpr unsigned STATE_BITS = 8 * sizeof(uint64_t);
+		static constexpr unsigned RESULT_BITS = 8 * sizeof(uint64_t);
 
-        uint64_t x;
-        uint64_t y;
+		uint64_t x;
+		uint64_t y;
 
 	public:
 		Random(uint64_t seed) noexcept;
@@ -30,7 +30,7 @@ namespace coyote
 		void seed(const uint64_t seed);
 
 		// Returns the next random number.
-        uint64_t next();
+		uint64_t next();
 
 	private:
 		static inline uint64_t rotl(const uint64_t x, const uint64_t k)
